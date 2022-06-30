6482eb102201cda76cbdad3c656a067abc8b69ee
davide_Lentini <davide.lentini000@gmail.com>
2022-06-30 15:56:47 +0200

    Add new files , script and installer

---
+++ install_ALL.bat
@@ -0,0 +1,4 @@
+python-3.10.5.exe
+pip install poetry
+nmap-7.92-setup.exe
+poetry install
---
+++ script.py
@@ -0,0 +1,38 @@
+import nmap
+import os
+import sys
+i = 0
+nm = nmap.PortScanner()
+array_of_valid_ip = []
+firmware = str(sys.argv[1])
+print(firmware)
+
+
+nm.scan(hosts='192.168.1.0/24', arguments='-n -sP')
+hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+print("ID DRONE ONLINE")
+for host, status in hosts_list:
+    arr = host.split(".")
+    
+    if(int(arr[3])>=11):
+        print("ID DRONE : " + arr[3] + " is online !")
+        array_of_valid_ip.append(host) #check last_byte of the ip_address , range is 11-255
+
+    
+    #print(host)
+print("OTA UPLOADING")
+size = len(array_of_valid_ip)
+print(" ")
+print(" ")
+print(" ")
+#create string with ips
+str_p= ""
+
+for i in range(size):
+    str_p +=(" -p " + array_of_valid_ip[i])
+
+# send command to cmd
+string = "poetry run ap-uploader " + (str_p) + " " + (firmware)
+os.system('cmd /k '+string)
+
+
