1b2845f27724ce3ca9b38660dda8bd2de5e80769
davide_Lentini <davide.lentini000@gmail.com>
2022-06-30 15:59:27 +0200

    Add new files , script and installer

--- script.py
+++ script.py
@@ -2,15 +2,21 @@ import nmap
 import os
 import sys
 i = 0
-nm = nmap.PortScanner()
 array_of_valid_ip = []
 firmware = str(sys.argv[1])
 print(firmware)
 
 
+nm = nmap.PortScanner()
+
+
+
+
 nm.scan(hosts='192.168.1.0/24', arguments='-n -sP')
 hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 print("ID DRONE ONLINE")
+
+#catch ips
 for host, status in hosts_list:
     arr = host.split(".")
     
@@ -18,14 +24,14 @@ for host, status in hosts_list:
         print("ID DRONE : " + arr[3] + " is online !")
         array_of_valid_ip.append(host) #check last_byte of the ip_address , range is 11-255
 
-    
-    #print(host)
+
+#print(host)
 print("OTA UPLOADING")
 size = len(array_of_valid_ip)
 print(" ")
 print(" ")
-print(" ")
 #create string with ips
+
 str_p= ""
 
 for i in range(size):
